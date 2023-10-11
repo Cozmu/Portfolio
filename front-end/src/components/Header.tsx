957f3fe3b3344069ce73bca780c889c9db2c84d3
Cozmu <www.jorgereis@gmail.com>
2023-10-11 13:54:17 -0300

    feat: colocando novo token em variavel de ambiente

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,24 +1,25 @@
 import { type ReactElement, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Octokit } from 'octokit';
 
 function Header(): ReactElement {
   useEffect(() => {
-    const x = async (): Promise<object> => {
-      const headers = {
-        Authorization:
-          // eslint-disable-next-line max-len
-          'Bearer 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',
-        'X-GitHub-Api-Version': '2022-11-28',
-      };
-      const a = await fetch('https://api.github.com/user/Cozmu/repos', {
-        method: 'GET',
-        headers,
+    const x = async (): Promise<{ data: object }> => {
+      const octokit = new Octokit({
+        // metodo para acessar variaveis de ambiente com vite
+        auth: import.meta.env.TOKEN,
+      });
+      const a = await octokit.request('GET /repos/Cozmu/Online-Store', {
+        headers: {
+          'content-type': 'text/plain',
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
       });
       return a;
     };
     x()
       .then((x) => {
-        console.log(x);
+        console.log(x.data);
       })
       .catch((error) => {
         return error;
