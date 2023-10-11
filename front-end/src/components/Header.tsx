526cec1aebb41695de1844d5be99dbf568e4345c
Cozmu <www.jorgereis@gmail.com>
2023-10-10 22:11:07 -0300

    fix: tentando utilizar api do github

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,7 +1,30 @@
-import { type ReactElement } from 'react';
+import { type ReactElement, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header(): ReactElement {
+  useEffect(() => {
+    const x = async (): Promise<object> => {
+      const headers = {
+        Authorization:
+          // eslint-disable-next-line max-len
+          'Bearer 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',
+        'X-GitHub-Api-Version': '2022-11-28',
+      };
+      const a = await fetch('https://api.github.com/user/Cozmu/repos', {
+        method: 'GET',
+        headers,
+      });
+      return a;
+    };
+    x()
+      .then((x) => {
+        console.log(x);
+      })
+      .catch((error) => {
+        return error;
+      });
+  }, []);
+
   return (
     <header>
       <NavLink to='/sobre'>Sobre</NavLink>
