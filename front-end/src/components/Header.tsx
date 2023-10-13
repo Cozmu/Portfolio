2a99570315e0081e192d90302ed0cc64a8cf1b86
Cozmu <www.jorgereis@gmail.com>
2023-10-13 16:17:00 -0300

    chore: ...

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,26 +1,27 @@
-import { useEffect, type ReactElement } from 'react';
+import { type ReactElement } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header(): ReactElement {
-  const x = async (): Promise<object> => {
-    // const endpoint = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image`;
-    // const result = await fetch(endpoint, {
-    //   headers: {
-    //     Authorization: 'Basic ' + btoa(`${apiKey}:${apiSecret}`),
-    //   },
-    // });
-    // return result;
-  };
+  // const x = async (): Promise<object> => {
+  //   // const endpoint =
+  // `https://api.cloudinary.com/v1_1/${cloudName}/resources/image`;
+  //   // const result = await fetch(endpoint, {
+  //   //   headers: {
+  //   //     Authorization: 'Basic ' + btoa(`${apiKey}:${apiSecret}`),
+  //   //   },
+  //   // });
+  //   // return result;
+  // };
 
-  useEffect(() => {
-    x()
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Erro ao acessar a API do Cloudinary:', error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   x()
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Erro ao acessar a API do Cloudinary:', error);
+  //     });
+  // }, []);
 
   return (
     <header>
