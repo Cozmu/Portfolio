be9c3b3f3e45fec293cd0d05ad94165109d30adc
Cozmu <www.jorgereis@gmail.com>
2023-10-23 19:18:36 -0300

    feat: tentando capiturar e descopactar arquivo zip da api

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,12 +1,28 @@
 import { type ReactElement, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import unzipper from 'unzipper';
 
 function Header(): ReactElement {
-  const [img, setImg] = useState<string>();
+  const [img, setImg] = useState<string[]>();
 
-  const response = async (): Promise<string> => {
-    const result = await fetch('http://localhost:3001/imagens/Delivery-App');
-    return result;
+  // fix
+  const response = async (): Promise<string[]> => {
+    const response = await fetch('http://localhost:3001/imagens/Delivery-App');
+    const blob = await response.blob();
+    const arrayBuffer = await blob.arrayBuffer();
+    const directory = await unzipper.Open.buffer(arrayBuffer);
+    const listaDeArquivos = Object.keys(directory.files)
+      .map((nomeDoArquivo) => {
+        const arquivo = directory.files[nomeDoArquivo];
+        if (arquivo.type === 'File') {
+          const blob = new Blob([arquivo._fs.readFileSync()]);
+          const urlDoArquivo = URL.createObjectURL(blob);
+          return urlDoArquivo;
+        }
+        return '';
+      })
+      .filter(Boolean);
+    return listaDeArquivos;
   };
 
   useEffect(() => {
@@ -18,8 +34,14 @@ function Header(): ReactElement {
         console.log(error);
       });
   }, []);
+
   return (
     <header>
+      {img?.map((urlDoArquivo, index) => (
+        <div key={index}>
+          <img src={urlDoArquivo} alt='' />
+        </div>
+      ))}
       <nav>
         <NavLink to='/'>Sobre</NavLink>
         <NavLink to='/projetos'>Projetos</NavLink>
