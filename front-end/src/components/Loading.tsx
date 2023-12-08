3a74ce9f21953fe38dccd78d5a9b71e42bc1f792
Cozmu <www.jorgereis@gmail.com>
2023-12-08 16:40:05 -0300

    feat: animação de loading finalizada

--- front-end/src/components/Loading.tsx
+++ front-end/src/components/Loading.tsx
@@ -2,12 +2,21 @@ import { type ReactElement } from 'react';
 
 function Loading(): ReactElement {
   return (
-    <div className='flex w-full items-center justify-center gap-1 h-80 loading-animation'>
-      <div className='rounded-full w-4 h-4 bg-contrast/75'></div>
-      <div className='rounded-full w-4 h-4 bg-contrast/75'></div>
-      <div className='rounded-full w-4 h-4 bg-contrast/75'></div>
+    <section className='flex w-full items-center justify-center gap-1 h-80'>
+      <div
+        className='rounded-full w-4 h-4 bg-contrast/75'
+        style={{ animation: 'pulse 1.5s infinite', animationDelay: '0s' }}
+      />
+      <div
+        className='rounded-full w-4 h-4 bg-contrast/75'
+        style={{ animation: 'pulse 1.5s infinite', animationDelay: '0.5s' }}
+      />
+      <div
+        className='rounded-full w-4 h-4 bg-contrast/75'
+        style={{ animation: 'pulse 1.5s infinite', animationDelay: '0.8s' }}
+      />
       {/* <p>Loading...</p> */}
-    </div>
+    </section>
   );
 }
 
