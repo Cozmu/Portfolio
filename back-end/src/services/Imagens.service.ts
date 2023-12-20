84954defdafde33f4228f2cb141b02821bdf51ee
Cozmu <www.jorgereis@gmail.com>
2023-12-20 20:37:26 -0300

    fix: corrigindo a falta da pasta src na rota do diretorio

--- back-end/src/services/Imagens.service.ts
+++ back-end/src/services/Imagens.service.ts
@@ -8,20 +8,23 @@ interface IgetPNGFilesDirectory {
 
 const getPNGFilesDirectory = (instruction: string[] | string): IgetPNGFilesDirectory => {
   const mainFilename = require.main?.filename
-
   const dirname = mainFilename ? path.dirname(mainFilename) : ''
-  const directory = path.join(dirname, '../imgs')
+
+  const directory = path.join(dirname, '..', 'src', 'imgs')
 
   if (typeof instruction === 'string') {
     const regex = new RegExp(instruction, 'i')
     const files = fs.readdirSync(directory).filter(arquivo => regex.test(arquivo))
+
     return { files, directory }
   }
   const files: string[] = []
 
   instruction.forEach(name => {
     const fileName = `${name}-2.png`
-    if (fs.existsSync(`${directory}/${fileName}`)) {
+    const filePath = path.join(directory, fileName)
+
+    if (fs.existsSync(filePath)) {
       files.push(fileName)
     } else {
       files.push('back-end.png')
