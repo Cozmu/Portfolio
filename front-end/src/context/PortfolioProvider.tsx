54ec20e853a6991b7f4af7079ddbb488f367ac70
Cozmu <www.jorgereis@gmail.com>
2023-12-28 19:31:39 -0300

    refactor: trocando requisição de projetos para provider

--- front-end/src/context/PortfolioProvider.tsx
+++ front-end/src/context/PortfolioProvider.tsx
@@ -1,7 +1,13 @@
 import { useMemo, useEffect, useState } from 'react';
 import PortfolioContext from './PortfolioContext';
 import { type IProps } from './interfaces/IContext';
-import { type Iproject } from '../service/APIgithub';
+import filterDate, { type Irequest, type Iproject } from '../service/APIgithub';
+import unzipFile, { type IunzipFile } from '../service/ImagesAPI';
+
+interface IserviceAPI {
+  projects: Irequest[];
+  projectsIMG: IunzipFile[];
+}
 
 function PortfolioProvider({ children }: IProps): JSX.Element {
   const [pulseContactCard, setPulseContactCard] = useState<boolean>(false);
@@ -11,6 +17,44 @@ function PortfolioProvider({ children }: IProps): JSX.Element {
   const [toggleBaseColors, setToggleBaseColors] = useState<string>(
     localBaseColors ?? 'dark',
   );
+
+  const serviceAPI = async (): Promise<IserviceAPI> => {
+    const projects = await filterDate();
+    const projectsIMG = await unzipFile('', 'projects');
+
+    return { projects, projectsIMG };
+  };
+
+  useEffect(() => {
+    serviceAPI()
+      .then(({ projects, projectsIMG }) => {
+        const result: Iproject[] = [];
+        projects.forEach((project) => {
+          const imgs = projectsIMG.find((img) => {
+            return img.fileName === `${project.name}-2`;
+          });
+
+          if (imgs) {
+            const allProperty = Object.assign({}, project, { img: imgs });
+            result.push(allProperty);
+          } else {
+            const allProperty = Object.assign({}, project, {
+              img: {
+                fileName: projectsIMG[0].fileName,
+                url: projectsIMG[0].url,
+              },
+            });
+            result.push(allProperty);
+          }
+        });
+        console.log('C', result);
+
+        setProjects(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   useEffect(() => {
     if (toggleBaseColors === 'dark') {
--- front-end/src/pages/Projects.tsx
+++ front-end/src/pages/Projects.tsx
@@ -1,61 +1,15 @@
-import { useContext, type ReactElement, useEffect, useState } from 'react';
+import { type ReactElement, useEffect, useState } from 'react';
 import Header from '../components/Header';
-import PortfolioContext from '../context/PortfolioContext';
-import filterDate, { type Irequest, type Iproject } from '../service/APIgithub';
-import unzipFile, { type IunzipFile } from '../service/ImagesAPI';
 import Loading from '../components/Loading';
 import ProjectsCards from '../components/ProjectsCards';
 
-interface IserviceAPI {
-  projects: Irequest[];
-  projectsIMG: IunzipFile[];
-}
-
 function Projects(): ReactElement {
   const [isLoading, setIsLoading] = useState(true);
-  const { setProjects } = useContext(PortfolioContext);
-
-  const serviceAPI = async (): Promise<IserviceAPI> => {
-    const projects = await filterDate();
-    const projectsIMG = await unzipFile('', 'projects');
-
-    return { projects, projectsIMG };
-  };
 
   useEffect(() => {
-    serviceAPI()
-      .then(({ projects, projectsIMG }) => {
-        const result: Iproject[] = [];
-        projects.forEach((project) => {
-          const imgs = projectsIMG.find((img) => {
-            return img.fileName === `${project.name}-2`;
-          });
-
-          if (imgs) {
-            const allProperty = Object.assign({}, project, { img: imgs });
-            result.push(allProperty);
-          } else {
-            const allProperty = Object.assign({}, project, {
-              img: {
-                fileName: projectsIMG[0].fileName,
-                url: projectsIMG[0].url,
-              },
-            });
-            result.push(allProperty);
-          }
-        });
-        console.log('C', result);
-
-        setProjects(result);
-        setTimeout(() => {
-          if (result.length > 0) {
-            setIsLoading(false);
-          }
-        }, 500);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1100);
   }, []);
 
   if (isLoading) {
