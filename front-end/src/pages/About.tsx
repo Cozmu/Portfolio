38071f22231fae937ceb94332d63a15b28c3b957
Cozmu <www.jorgereis@gmail.com>
2024-01-25 17:39:31 -0300

    feat: sobre mim implementado

--- front-end/src/pages/About.tsx
+++ front-end/src/pages/About.tsx
@@ -8,25 +8,49 @@ function About(): ReactElement {
     <>
       <Header />
       <div className='flex p-6 mx-10 pt-40 gap-10 justify-center'>
-        <section className='w-1/2 flex flex-col gap-2'>
+        <section className='w-1/2 flex flex-col gap-2 flex-wrap'>
           <p className='text-lg dark:text-contrast text-tertiary tex'>
             . . /sobre mim
           </p>
-          <h1 className='text-slate-50 text-5xl'>
+          <h1 className='dark:text-slate-50 text-black text-5xl'>
             Sou um desenvolvedor full-stack no início da minha carreira, mas com
             muitos sonhos e força de vontade.
           </h1>
-          <div>
-            <p>infancia: primeiros contatos com a tecnologia e sonhos</p>
-            <p>
-              formação: como cheguei ao ponto de querer seguir essa carreira e
-              como adiquiri meus conhecimentos que eu tenho hoje
+          <div className='flex flex-col w-full mt-10 gap-8'>
+            <p
+            /* infancia: primeiros contatos com a tecnologia e sonhos */
+            >
+              A tecnologia sempre foi a minha paixão, desde a infância. Eu
+              passava horas na frente do computador, jogando clássicos como GTA
+              San Andreas, Donkey Kong e Call of Duty, navegando na internet em
+              busca de novidades para conversar e compartilhar com meus amigos
+              nas redes sociais e fazendo trabalhos escolares com Word ou
+              PowerPoint, que me introduziram ao uso de ferramentas digitais.
             </p>
-            <p>
-              objetivos: como eu quero ser daqui pra frente e como eu posso
-              agregar valor
+            <p
+            /* formação: como cheguei ao ponto de querer seguir essa carreira e
+              como adiquiri meus conhecimentos que eu tenho hoje */
+            >
+              Quando conclui o ensino médio, estava claro que meu futuro
+              profissional envolveria tecnologia. Decidi, então, focar nos
+              estudos em programação e me matriculei na Trybe, uma escola
+              especializada em desenvolvimento web. Com dedicação e empenho, fui
+              aprovado no processo seletivo e tive a oportunidade de aprimorar
+              meus conhecimentos em diversas tecnologias, aplicando-os em
+              projetos práticos e desafiadores.
             </p>
-            <p></p>
+            <p
+            /* objetivos: como eu quero ser daqui pra frente e como eu posso
+              agregar valor */
+            >
+              Atualmente, estou dedicado aperfeiçoando e trabalhando em projetos
+              que agreguem valor ao meu portfólio e a mim como profissional,
+              pois sei que ainda estou no início da minha carreira e tenho muito
+              a conquistar e desenvolver. A paixão pela tecnologia continua me
+              impulsionando, assim como a busca por novos desafios que permitem
+              expandir meus conhecimentos e habilidades. Estou comprometido em
+              crescer e evoluir constantemente nesta área que tanto me fascina.
+            </p>
           </div>
         </section>
         <section className='w-1/2  flex flex-col justify-between items-center'>
