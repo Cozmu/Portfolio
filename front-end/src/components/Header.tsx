0a9a4268a58795f3f952ef03bd56a2aa59a1dc0a
Cozmu <www.jorgereis@gmail.com>
2024-01-29 17:39:04 -0300

    feat: reposicionando botao de menu

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -64,41 +64,46 @@ function Header({ home }: IPropsHeader): ReactElement {
     <header
       id='header-container'
       className={`flex fixed items-center pb-28
-        ${!home && 'flex-row-reverse'}
+        ${!home && 'flex-row-reverse'} p-6
         duration-500 ease-in-out transition-opacity opacity-100
-        justify-between p-6
+        justify-between 
         xl:mx-10 xl:w-[85%] lg:mx-8 lg:w-[81.8%] md:w-[86%] w-full
         `}
     >
-      <div>
+      <section className='flex justify-end gap-2'>
         <button
-          className='rounded-full w-14 h-10 flex justify-center items-center
-            dark:hover:ring-1 hover:bg-slate-50 dark:hover:bg-zinc-900
-          dark:hover:ring-zinc-700 shadow-md
-            dark:shadow dark:shadow-zinc-800 dark:bg-zinc-900'
-          onClick={() => {
-            setToggleBaseColors(toggleBaseColors === 'dark' ? 'light' : 'dark');
-          }}
-        >
-          {toggleBaseColors === 'dark' ? (
-            <BsMoonStars />
-          ) : (
-            <MdOutlineWbSunny className='text-contrast' />
-          )}
-        </button>
-      </div>
-      <button
-        className={`
+          // onClick={}
+          className={`
           dark:bg-zinc-900 h-10 px-4 py-2 rounded-full  
           flex items-center gap-1 justify-center 
           md:hidden
           dark:text-slate-50 dark:shadow dark:shadow-zinc-800
           shadow-box-shadow text-black
         `}
-      >
-        <span className='ml-1'>Menu</span>
-        <MdOutlineKeyboardArrowDown />
-      </button>
+        >
+          <span className='ml-1'>Menu</span>
+          <MdOutlineKeyboardArrowDown />
+        </button>
+        <div>
+          <button
+            className='rounded-full w-14 h-10 flex justify-center items-center
+            dark:hover:ring-1 hover:bg-slate-50 dark:hover:bg-zinc-900
+          dark:hover:ring-zinc-700 shadow-md
+            dark:shadow dark:shadow-zinc-800 dark:bg-zinc-900'
+            onClick={() => {
+              setToggleBaseColors(
+                toggleBaseColors === 'dark' ? 'light' : 'dark',
+              );
+            }}
+          >
+            {toggleBaseColors === 'dark' ? (
+              <BsMoonStars />
+            ) : (
+              <MdOutlineWbSunny className='text-contrast' />
+            )}
+          </button>
+        </div>
+      </section>
       <nav
         className=' items-center justify-around
           md:w-2/5 lg:w-1/3 xl:w-1/4
@@ -108,7 +113,7 @@ function Header({ home }: IPropsHeader): ReactElement {
           dark:text-slate-50 dark:bg-zinc-900
           dark:shadow dark:shadow-zinc-800'
       >
-        <div className='hidden'></div>
+        <div className='md:hidden'></div>
         <NavLink
           className={`${
             pathname === '/about' && 'text-tertiary'
@@ -154,25 +159,27 @@ function Header({ home }: IPropsHeader): ReactElement {
           )}
         </NavLink>
       </nav>
-      <NavLink className='' to='/'>
-        <div
-          id='profile-picture'
-          className={`rounded${
-            isHome ? '' : '-full'
-          } flex items-center justify-center
+      <section>
+        <NavLink className='' to='/'>
+          <div
+            id='profile-picture'
+            className={`rounded${
+              isHome ? '' : '-full'
+            } flex items-center justify-center
           ${isHome && animation}
           w-10 h-10 shadow-box-shadow bg-slate-50  dark:bg-zinc-800`}
-        >
-          <img
-            id='profile-picture'
-            className={`object-cover rounded${
-              isHome ? '' : '-full w-9 h-9'
-            } w-10 h-10 duration-700 ease-in-out`}
-            src={perfil}
-            alt='perfil'
-          />
-        </div>
-      </NavLink>
+          >
+            <img
+              id='profile-picture'
+              className={`object-cover rounded${
+                isHome ? '' : '-full w-9 h-9'
+              } w-10 h-10 duration-700 ease-in-out`}
+              src={perfil}
+              alt='perfil'
+            />
+          </div>
+        </NavLink>
+      </section>
     </header>
   );
 }
